794b2695975f626eebf609856e8af563ef2fe9c8
Moi <a.aguilar.moises@gmail.com>
2021-10-23 19:48:22 -0400

    Fix mutation observer to handle mobile safari

---
+++ vid_manager/static/vid_manager/video_info.js
@@ -0,0 +1,27 @@
+var appleWebKit = navigator.userAgent.includes("AppleWebKit/6")
+var vid = document.querySelector('video')
+
+const observerOptions = {
+        attributes: true
+};
+
+
+if (appleWebKit == true) {
+    var observer = new WebKitMutationObserver(callback);
+} else {
+    var observer = new MutationObserver(callback);
+}
+
+function callback(mutationList, observer) {
+    mutationList.forEach( (mutation) => {
+        switch(mutation.type) {
+            case 'attributes':
+                if (mutation.attributeName === 'src') {
+                    get_info(mutation.target.src)
+                }
+                break;
+        }
+    })
+};
+
+observer.observe(vid, observerOptions);
